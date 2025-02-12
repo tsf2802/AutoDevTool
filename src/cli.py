6f4356d80a761dd8905bdc7c4ac3009f386c3541
Anty120 <anthonyhug222@gmail.com>
2025-02-12 09:42:16 -0500

    successfully able to create django project by running the command 'python src/cli.py init --framework=django'

---
+++ src/cli.py
@@ -0,0 +1,25 @@
+import argparse
+import subprocess
+
+def create_project(args):
+    if args.framework == "django":
+        subprocess.run(["pip", "install", "django"])
+        subprocess.run(["django-admin", "startproject", "myproject"])
+    elif args.framework == "nextjs":
+        print("Creating a Next.js project... Not really tho, still need to code that part")
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="command")
+    
+    init_parser = subparsers.add_parser("init")
+    init_parser.add_argument("--framework", help="The name of the project to be initialized")
+
+    args = parser.parse_args()
+
+    if args.command == "init":
+        create_project(args)
+
+if __name__ == "__main__":
+    main()
