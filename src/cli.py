89bd65f599a04830627bc77a1cc8d96e5e54822d
Anty120 <anthonyhug222@gmail.com>
2025-02-14 09:14:09 -0500

    added example for integration of gemini

--- src/cli.py
+++ src/cli.py
@@ -1,17 +1,18 @@
 import argparse
 import subprocess
+from google import genai
 
 def create_project(args):
-    if args.framework == "django":
+    if args.framework == 'django':
         try:
-            subprocess.run(["pip", "show", "django"], capture_output=True, text=True, check=True)
+            subprocess.run(['pip', 'show', 'django'], capture_output=True, text=True, check=True)
         except subprocess.CalledProcessError:
-            print("Django is not installed. We are installing it now.")
-            subprocess.run(["pip", "install", "django"])
+            print('Django is not installed. We are installing it now.')
+            subprocess.run(['pip', 'install', 'django'])
         
-        subprocess.run(["django-admin", "startproject", "myproject"])
-    elif args.framework == "nextjs":
-        print("Creating a Next.js project... Not really tho, still need to code that part")
+        subprocess.run(['django-admin', 'startproject', 'myproject'])
+    elif args.framework == 'nextjs':
+        print('Creating a Next.js project... Not really tho, still need to code that part')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -28,4 +29,27 @@ def main():
 
 if __name__ == "__main__":
     # run this code by doing 'python src/cli.py init --framework=django' in command line
+
+    client = genai.Client(api_key="")
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="""
+        Write a docstring for the following code, without adding extra empty lines for formatting (just line after line) and don't add an example of the usage of the function, but make sure to add explanation of args and any errors that might occur and don't add anything like ```python, but instead simply return the function with the docstring: 
+        def create_project(args):
+    if args.framework == 'django':
+        try:
+            subprocess.run(['pip', 'show', 'django'], capture_output=True, text=True, check=True)
+        except subprocess.CalledProcessError:
+            print('Django is not installed. We are installing it now.')
+            subprocess.run(['pip', 'install', 'django'])
+        
+        subprocess.run(['django-admin', 'startproject', 'myproject'])
+    elif args.framework == 'nextjs':
+        print('Creating a Next.js project... Not really tho, still need to code that part')
+        """,
+    )
+
+    print(response.text)
+
     main()
